b5d37b7963515a4adfdf376cf24950572647547d
Mohammed Shaheen <102543131+mohammedshaheen187@users.noreply.github.com>
2022-08-26 19:58:10 +0300

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Mohammed Shaheen on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
